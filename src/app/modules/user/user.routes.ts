3f7fc7a5042eb28f61396f310eded6d207fc8ff6
samssjubair <samssjubair@gmail.com>
2023-09-01 20:44:12 +0600

    authorization for user route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validations';
@@ -7,24 +9,24 @@ const router = express.Router();
 
 router.get(
   '/',
-  // auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.STUDENT),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.getAllFromDB
 );
 
-router.get('/:id', UserController.getByIdFromDB);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getByIdFromDB);
 
 
 
 router.patch(
   '/:id',
-  // auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.STUDENT),
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidation.update),
   UserController.updateIntoDb
 );
 
 router.delete(
     '/:id',
-    // auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.STUDENT),
+    auth(ENUM_USER_ROLE.ADMIN),
     UserController.deleteFromDB
 )
 
